75eea38bee751e2aca044b9d7f6debef0d2ab69d
sosualfred <sosualfred@gmail.com>
2020-10-11 13:43:13 +0000

    Basic Design of Years Page implemented.

---
+++ lib/config/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+final Color primaryColor = Color(0xFFe0e0e0);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
       title: 'ANU CGPA CALCULATOR',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        accentColor: Colors.yellow,
+        primaryColor: Colors.yellow[800],
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: YearsPage(),
@@ -20,26 +22,126 @@ class MyApp extends StatelessWidget {
 }
 
 class YearsPage extends StatelessWidget {
+  final String graduateSvgPath = 'assets/images/graduate.svg';
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Years"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return Drawer(
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            iconTheme: new IconThemeData(color: Colors.yellow[700]),
+            backgroundColor: Color(0xFFF5F5F5),
+            elevation: 0,
+            title: Text(
+              "Years",
+              style: TextStyle(color: Colors.yellow[700]),
             ),
-          ],
+            actions: [
+              Icon(
+                Icons.info_outline,
+                color: Colors.yellow[700],
+              ),
+            ],
+          ),
+          drawer: Drawer(
+            child: Text('Hello'),
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Container(
+                    height: 150,
+                    width: MediaQuery.of(context).size.width * 0.95,
+                    child: Stack(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(
+                              left: MediaQuery.of(context).size.width * 0.25,
+                              top: 10),
+                          child: SvgPicture.asset(
+                            graduateSvgPath,
+                            height: 140,
+                            color: Color.fromRGBO(255, 193, 4, 0.3),
+                          ),
+                        ),
+                        Container(
+                          color: Color.fromRGBO(255, 193, 4, 0.2),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              color: Colors.yellow[700],
+                              width: MediaQuery.of(context).size.width * 0.25,
+                              alignment: Alignment.center,
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text(
+                                    "Year",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  Text(
+                                    "1",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 64,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.4,
+                              alignment: Alignment.center,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "CGPA",
+                                    style: TextStyle(
+                                      color: Colors.yellow[700],
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Text(
+                                    "4.00",
+                                    style: TextStyle(
+                                        color: Colors.yellow[700],
+                                        fontSize: 54,
+                                        fontWeight: FontWeight.normal),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
