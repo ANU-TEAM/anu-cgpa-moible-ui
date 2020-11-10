4321a207165a8aac7ab8679de9ca78edfd1b06a7
sosualfred <sosualfred@gmail.com>
2020-11-10 15:23:42 +0000

    Remove getit library. Add click to login button.

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -8,66 +8,88 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GestureDetector(
-        onTap: () async {
-          bool weCanCheckBiometrics = await localAuth.canCheckBiometrics;
-          if (weCanCheckBiometrics) {
-            bool authenticated = await localAuth.authenticateWithBiometrics(
-              localizedReason: "",
-            );
+      body: Container(
+        color: Colors.yellow[700],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Icon(
+                  Icons.fingerprint,
+                  size: 124.0,
+                  color: Colors.white,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  child: FlatButton(
+                    onPressed: () async {
+                      bool weCanCheckBiometrics =
+                          await localAuth.canCheckBiometrics;
+                      if (weCanCheckBiometrics) {
+                        bool authenticated =
+                            await localAuth.authenticateWithBiometrics(
+                          localizedReason: "",
+                        );
 
-            if (authenticated) {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => OverviewScreen()));
-            }
-          } else {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => OverviewScreen()));
-          }
-        },
-        child: Container(
-          color: Colors.yellow[700],
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: Icon(
-                    Icons.fingerprint,
-                    size: 124.0,
+                        if (authenticated) {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => OverviewScreen()));
+                        }
+                      } else {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => OverviewScreen()));
+                      }
+                    },
+                    height: 55,
+                    color: Colors.yellow[800],
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "CLICK TO LOGIN",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 17,
+                          ),
+                        ),
+                      ],
+                    ),
+                    // child: Text(
+                    //   "Touch to Login",
+                    //   textAlign: TextAlign.center,
+                    //   style: TextStyle(
+                    //     color: Colors.white,
+                    //     fontSize: 30,
+                    //     fontWeight: FontWeight.bold,
+                    //   ),
+                    // ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(
+                  horizontal: 10,
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  "Once you have authenticated. \n The App will not ask you to authenticate until you close the app completly.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
                     color: Colors.white,
+                    fontSize: 18,
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: Text(
-                    "Touch to Login",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(
-                    horizontal: 10,
-                  ),
-                  alignment: Alignment.center,
-                  child: Text(
-                    "Once you have authenticated. \n The App will not ask you to authenticate until you close the app completly.",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
